79c4af13e6c27f1f02f882a2b008ec7d92f11a14
sidhesh <2016sidhesh.kanojia@ves.ac.in>
2019-11-25 01:49:47 +0530

    creating form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RequestPlaylistComponent } from './request-playlist/request-playlist.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'request-playlist',
+    component: RequestPlaylistComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { VideoPlaylistPlayerComponent } from './video-playlist/video-playlist-pl
 import { HttpRequestInterceptor } from './http-request-interceptor';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CustomUppercasePipe } from './custom-uppercase.pipe';
+import { RequestPlaylistComponent } from './request-playlist/request-playlist.component';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     VideoPlaylistComponent,
     VideoPlaylistPlayerComponent,
-    CustomUppercasePipe
+    CustomUppercasePipe,
+    RequestPlaylistComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
